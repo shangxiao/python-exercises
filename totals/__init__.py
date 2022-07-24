6ad17ca958015dbdcb581ce7c57f35a83dbc40d7
David Sanders <>
2022-07-24 14:15:11 +1000

    Generator expression

--- totals/__init__.py
+++ totals/__init__.py
@@ -3,13 +3,11 @@ def calculate_total_cost():
     Lessons:
         1. Augmented assignment:  https://docs.python.org/3/reference/simple_stmts.html#augmented-assignment-statements
         2. Builtin sum():  https://docs.python.org/3/library/functions.html#sum
-        3. List comprehension:  https://docs.python.org/3/howto/functional.html#generator-expressions-and-list-comprehensions
+        3. List comprehension/generator expression:  https://docs.python.org/3/howto/functional.html#generator-expressions-and-list-comprehensions
     """
     return sum(
-        [
-            calculate_category_total(category)
-            for category in ["rent", "utilities", "ingredients"]
-        ]
+        calculate_category_total(category)
+        for category in ["rent", "utilities", "ingredients"]
     )
 
 
