2ab50c28ac07365aad7678ad454ea335cc5ab5ae
David Sanders <>
2022-07-24 14:12:43 +1000

    List comprehension

--- totals/__init__.py
+++ totals/__init__.py
@@ -3,10 +3,12 @@ def calculate_total_cost():
     Lessons:
         1. Augmented assignment:  https://docs.python.org/3/reference/simple_stmts.html#augmented-assignment-statements
         2. Builtin sum():  https://docs.python.org/3/library/functions.html#sum
+        3. List comprehension:  https://docs.python.org/3/howto/functional.html#generator-expressions-and-list-comprehensions
     """
-    totals = []
-    for category in ["rent", "utilities", "ingredients"]:
-        totals.append(calculate_category_total(category))
+    totals = [
+        calculate_category_total(category)
+        for category in ["rent", "utilities", "ingredients"]
+    ]
 
     return sum(totals)
 
