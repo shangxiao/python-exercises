9e1d120203328cee71a178761b2c312137ff3463
David Sanders <>
2022-07-24 15:46:15 +1000

    any

--- member_of_group/__init__.py
+++ member_of_group/__init__.py
@@ -3,9 +3,6 @@ def is_member_of_group(name, *groups):
     Lessons:
         1. in operator:  https://docs.python.org/3/library/stdtypes.html#common-sequence-operations
                          https://docs.python.org/3/reference/expressions.html#in
+        2. any:  https://docs.python.org/3/library/functions.html#any
     """
-    for group in groups:
-        if name in group:
-            return True
-
-    return False
+    return any(name in group for group in groups)
