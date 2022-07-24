1762937d7f7ca8d07d91a818c8cf771ed0757e22
David Sanders <>
2022-07-24 15:42:36 +1000

    in operator

--- member_of_group/__init__.py
+++ member_of_group/__init__.py
@@ -1,10 +1,14 @@
 def is_member_of_group(name, *groups):
+    """
+    Lessons:
+        1. in operator:  https://docs.python.org/3/library/stdtypes.html#common-sequence-operations
+                         https://docs.python.org/3/reference/expressions.html#in
+    """
     is_member = False
 
     for group in groups:
-        for member in group:
-            if name == member:
-                is_member = True
-                break
+        if name in group:
+            is_member = True
+            break
 
     return is_member
