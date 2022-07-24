d17e7c4f22cb2469f389635e54b560511c84c121
David Sanders <>
2022-07-24 15:42:58 +1000

    Refactor: Simply return if value found

--- member_of_group/__init__.py
+++ member_of_group/__init__.py
@@ -4,11 +4,8 @@ def is_member_of_group(name, *groups):
         1. in operator:  https://docs.python.org/3/library/stdtypes.html#common-sequence-operations
                          https://docs.python.org/3/reference/expressions.html#in
     """
-    is_member = False
-
     for group in groups:
         if name in group:
-            is_member = True
-            break
+            return True
 
-    return is_member
+    return False
