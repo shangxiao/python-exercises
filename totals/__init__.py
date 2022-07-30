f9b51a221d40e848699fa5c7bf6a23f870b210f9
David Sanders <>
2022-07-30 15:52:45 +1000

    Augmented assignment

--- totals/__init__.py
+++ totals/__init__.py
@@ -1,8 +1,12 @@
 def calculate_total_cost():
+    """
+    Lessons:
+        1. Augmented assignment:  https://docs.python.org/3/reference/simple_stmts.html#augmented-assignment-statements
+    """
     total = 0
     categories = ["rent", "utilities", "ingredients"]
     for category in categories:
-        total = total + calculate_category_total(category)
+        total += calculate_category_total(category)
 
     return total
 
